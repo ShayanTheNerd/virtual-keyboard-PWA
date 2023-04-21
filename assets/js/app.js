9251760bb1ad3b86b63257ea772cb829351ad457
ShayanTheNerd <sh.zamani384@gmail.com>
2023-04-21 12:48:54 +0330

    Removed the push notification on theme change

--- assets/js/app.js
+++ assets/js/app.js
@@ -115,6 +115,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	detectDeviceAndDisplayWarn();
 
 	focusOnTextarea();
+
+	// registering Service Worker
+	navigator.serviceWorker.register('./assets/js/service-worker-PWA.mjs'); // URL from the project root
 });
 
 // handling events
@@ -155,20 +158,3 @@ document.addEventListener('DOMContentLoaded', () => {
 	['click', 'keydown'].forEach(event => window.addEventListener(event, simulateTyping));
 	window.addEventListener('keyup', () => document.querySelector('.key--pressed')?.classList.remove('key--pressed'));
 }
-
-// registering Service Worker
-window.addEventListener('load', () => navigator.serviceWorker.register('./assets/js/service-worker-PWA.mjs')); // URL from the root of project)
-
-// requesting permission for notifications
-document.getElementById('changeThemeBtn').addEventListener('click', () => {
-	const randomUUIDNotification = () => {
-		const notifTitle = 'Theme Changed';
-		const notifBody = `Random UUID: ${crypto.randomUUID()}`;
-		const notifImg = '../../favicon.ico';
-		const options = { body: notifBody, icon: notifImg };
-
-		return new Notification(notifTitle, options);
-	};
-
-	Notification.requestPermission().then(result => result === 'granted' && randomUUIDNotification());
-});
